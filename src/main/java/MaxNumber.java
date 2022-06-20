baf95564a098307115fbf23228b22bf2e4a18662
VladimirKovalev <valodb9gn@yandex.ru>
2022-06-20 15:04:30 +0200

    max&min&avg

--- src/main/java/MaxNumber.java
+++ src/main/java/MaxNumber.java
@@ -6,11 +6,25 @@
 
 public class MaxNumber {
     public static void main(String[] args) {
-        int number = 100;
+        int number = 10;
         double[] array = new double[number];
         for (int i = 0; i < array.length; i++) {
-            array[number] = Math.random();
+            array[i] = Math.random();
+            System.out.println(array[i]);
         }
+        double max = array[0];
+        double min = array[0];
+        double avg = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i])
+                max = array[i];
+            if (min > array[i])
+                min = array[i];
+            avg += array[i] / array.length;
+        }
+        System.out.println(" Максимальное число равно " + max);
+        System.out.println(" Минимальное число равно " + min);
+        System.out.println(" Среднее занчение равно " + avg);
 
     }
 }
